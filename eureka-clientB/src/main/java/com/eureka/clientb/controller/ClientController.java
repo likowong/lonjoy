f5917a31f5f41410d342c9bedf16db052ae72bb6
luke <15010343670@163.com>
2020-11-30 08:57:44 +0800

    init

--- eureka-clientB/src/main/java/com/eureka/clientb/controller/ClientController.java
+++ eureka-clientB/src/main/java/com/eureka/clientb/controller/ClientController.java
@@ -1,4 +1,4 @@
-package jumper.eureka.clientb.controller;
+package com.eureka.clientb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
