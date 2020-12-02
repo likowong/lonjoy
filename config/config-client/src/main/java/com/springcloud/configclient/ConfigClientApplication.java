6e27dab2653b81110f30aeb3f78cc6f560339bb8
luke <15010343670@163.com>
2020-12-02 09:30:44 +0800

    init

--- config/config-client/src/main/java/com/springcloud/configclient/ConfigClientApplication.java
+++ config/config-client/src/main/java/com/springcloud/configclient/ConfigClientApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+/**
+ * @author liko
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
